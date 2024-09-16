fa8940cc0b7beaaaf27819f455cb9a4330f9c12e
Benjamin Corwin <bscorwin@gmail.com>
2024-09-15 17:01:51 -0700

    reformat main

--- wiki_web/main.py
+++ wiki_web/main.py
@@ -1,17 +1,21 @@
-# TODO: add pre-commit hooks
 import logging
 
 from wiki_web import wikiWeb
 
 logging.basicConfig(
-  level=logging.INFO,
-  format="%(asctime)s\t| %(levelname)s\t| %(name)s.%(funcName)s.%(lineno)d\t| %(message)s",
-  datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    format="\t| ".join(
+        "%(asctime)s",
+        "%(levelname)s",
+        "%(name)s.%(funcName)s.%(lineno)d",
+        "%(message)s",
+    ),
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
 
 ww = wikiWeb()
-ww.load_web()
-ww.add_seeds(100)
+# ww.load_web()
+ww.add_seeds(10)
 ww.build_web()
 ww.save_web()
