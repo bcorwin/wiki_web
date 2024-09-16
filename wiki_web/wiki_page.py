a4729e4721b821c934f1502b2616dcd65ec7b2bc
Benjamin Corwin <bscorwin@gmail.com>
2024-09-16 10:18:14 -0700

    reformat files

--- wiki_web/wiki_page.py
+++ wiki_web/wiki_page.py
@@ -3,94 +3,94 @@ import requests
 import logging
 from bs4 import BeautifulSoup, element
 
-URL_NAME_REGEX = r'\/([^\/]+)$'
+URL_NAME_REGEX = r"\/([^\/]+)$"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def remove_elements(soup: BeautifulSoup, tag:str, class_name:str = None):
-  ### Remove elements based on tag and class (optional)
-  for e in soup.find_all(tag, class_ = class_name):
-    e.decompose()
 
-def is_in_parenthesis(sub_element: element.Tag, element:element.Tag):
-  ### Check if sub_element is surrounded by parenthesis in elementn text
-  parenthesis_regex = fr'\([^\)]*{sub_element}[^\)]*\)'
-  return re.search(parenthesis_regex, str(element)) is not None
+def remove_elements(soup: BeautifulSoup, tag: str, class_name: str = None):
+    # Remove elements based on tag and class (optional)
+    for e in soup.find_all(tag, class_=class_name):
+        e.decompose()
+
+
+def is_in_parenthesis(sub_element: element.Tag, element: element.Tag):
+    # Check if sub_element is surrounded by parenthesis in elementn text
+    parenthesis_regex = rf"\([^\)]*{sub_element}[^\)]*\)"
+    return re.search(parenthesis_regex, str(element)) is not None
+
 
 def is_valid_link(element: element.Tag):
-  ### Checks if the link is valid per the getting to philosphy rules
-  classes = element.get("class", [])
-  href = element.get("href")
+    # Checks if the link is valid per the getting to philosphy rules
+    classes = element.get("class", [])
+    href = element.get("href")
 
-  # Internal links don't have a class or are mw-redirect
-  if href.startswith("#cite"):
+    # Internal links don't have a class or are mw-redirect
+    if href.startswith("#cite"):
+        return False
+    if len(classes) == 0 or "mw-redirect" in classes:
+        return True
     return False
-  if len(classes) == 0 or "mw-redirect" in classes:
-    return True
-  return False
 
-class wikiPage():
-  def __init__(self, url: str):
-    self.url = url
-    self.name = re.search(URL_NAME_REGEX, url).group(1)
-    self._first_link = None
 
-  def __str__(self):
-    return self.url
-  
-  def __repr__(self):
-    return str(self)
+class wikiPage:
+    def __init__(self, url: str):
+        self.url = url
+        self.name = re.search(URL_NAME_REGEX, url).group(1)
+        self._first_link = None
 
-  def __hash__(self):
-    return hash(self.name)
-  
-  def __eq__(self, other):
-    return self.name == other.name
-  
-  def __ne__(self, other):
-    return not(self == other)
+    def __str__(self):
+        return self.url
 
-  def _get_first_link(self, html_content):
-    # TODO: break this into helper functions and test them
-    elements_to_check = ["p", "ol", "ul"]
-    soup = BeautifulSoup(html_content, "html.parser")
-    
-    # Remove various elements
-    objs_to_remove = [
-      {"tag": "i"},
-      {"tag": "div", "class_name": "navbar"},
-      {"tag": "table", "class_name": "infobox"},
-      {"tag": "table", "class_name": "sidebar"},
-    ]
-    for obj in objs_to_remove:
-      remove_elements(soup, **obj)
+    def __repr__(self):
+        return str(self)
 
-    # Get the main page
-    main_page = (
-      soup
-      .select_one("#mw-content-text")
-      .find_all(elements_to_check)
-    )
+    def __hash__(self):
+        return hash(self.name)
 
-    first_link = None
-    for p in main_page:
-      # Check links in order
-      for a in p.find_all("a", href=True):
-        if is_valid_link(a) and not is_in_parenthesis(a, p):
-          first_link = "https://en.wikipedia.org" + a.get("href")
-          return first_link
-    return "No valid link found."
-      
-  def get_first_link(self):
-    # If I've already found this link, return it. Otherwise, find it.
-    if not self._first_link:
-      res = requests.get(self.url)
-      if res.status_code != 200:
-        first_link = res.reason
-      else:
-        logger.debug(res.url)
-        first_link = self._get_first_link(res.content)
+    def __eq__(self, other):
+        return self.name == other.name
 
-      self._first_link = first_link
-    return self._first_link
+    def __ne__(self, other):
+        return not (self == other)
+
+    def _get_first_link(self, html_content):
+        # TODO: break this into helper functions and test them
+        elements_to_check = ["p", "ol", "ul"]
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        # Remove various elements
+        objs_to_remove = [
+            {"tag": "i"},
+            {"tag": "div", "class_name": "navbar"},
+            {"tag": "table", "class_name": "infobox"},
+            {"tag": "table", "class_name": "sidebar"},
+        ]
+        for obj in objs_to_remove:
+            remove_elements(soup, **obj)
+
+        # Get the main page
+        main_page = soup.select_one("#mw-content-text").find_all(elements_to_check)
+
+        first_link = None
+        for p in main_page:
+            # Check links in order
+            for a in p.find_all("a", href=True):
+                if is_valid_link(a) and not is_in_parenthesis(a, p):
+                    first_link = "https://en.wikipedia.org" + a.get("href")
+                    return first_link
+        return "No valid link found."
+
+    def get_first_link(self):
+        # If I've already found this link, return it. Otherwise, find it.
+        if not self._first_link:
+            res = requests.get(self.url)
+            if res.status_code != 200:
+                first_link = res.reason
+            else:
+                logger.debug(res.url)
+                first_link = self._get_first_link(res.content)
+
+            self._first_link = first_link
+        return self._first_link
--- wiki_web/wiki_web.py
+++ wiki_web/wiki_web.py
@@ -10,59 +10,61 @@ RANDOM_URL = "https://en.wikipedia.org/wiki/Special:Random"
 
 logger = logging.getLogger(__name__)
 
+
 def random_wiki_page():
-  res = requests.get(RANDOM_URL)
-  return res.url
+    res = requests.get(RANDOM_URL)
+    return res.url
 
-class wikiWeb():
-  def __init__(self):
-    logger.info("Wiki Web initialized.")
-    self.first_links = {}
 
-  def _add_page(self, page: wikiPage):
-    first_link = wikiPage(page.get_first_link())
-    self.first_links[page] = first_link
-    if first_link not in self.first_links:
-      self._add_page(first_link)
-    else:
-      logger.info(f"Stopping at {first_link}")
+class wikiWeb:
+    def __init__(self):
+        logger.info("Wiki Web initialized.")
+        self.first_links = {}
 
-  def add_urls(self, urls:list | str):
-    if isinstance(urls, str):
-      urls = [urls]
-    logger.info(f"Adding {len(urls)} url(s).")
-    for url in urls:
-      seed = wikiPage(url)
-      self.first_links[seed] = None
+    def _add_page(self, page: wikiPage):
+        first_link = wikiPage(page.get_first_link())
+        self.first_links[page] = first_link
+        if first_link not in self.first_links:
+            self._add_page(first_link)
+        else:
+            logger.info(f"Stopping at {first_link}")
 
-  def add_seeds(self, n: int = 100):
-    logger.info(f"Adding {n} seed(s).")
-    for _ in range(n):
-      seed = wikiPage(url = random_wiki_page())
-      self.first_links[seed] = None
+    def add_urls(self, urls: list | str):
+        if isinstance(urls, str):
+            urls = [urls]
+        logger.info(f"Adding {len(urls)} url(s).")
+        for url in urls:
+            seed = wikiPage(url)
+            self.first_links[seed] = None
 
-  def build_web(self):
-    logger.info("Building the web.")
-    seeds = [key for key, item in self.first_links.items() if item is None]
-    for seed in seeds:
-        logger.info(f"Clicking through {seed}")
-        self._add_page(seed)
+    def add_seeds(self, n: int = 100):
+        logger.info(f"Adding {n} seed(s).")
+        for _ in range(n):
+            seed = wikiPage(url=random_wiki_page())
+            self.first_links[seed] = None
 
-  def load_web(self, file_name="wiki_web.tsv"):
-    ### Loads a previously saved web. Helpful for adding more samples
-    logger.info(f"Loading {file_name}.")
-    with open(file_name, "r") as file:
-      reader = csv.reader(file, delimiter="\t")
-      n = 0
-      for row in reader:
-        self.first_links[row[0]] = row[1]
-        n += 1
-      logger.info(f"{n} urls added")
+    def build_web(self):
+        logger.info("Building the web.")
+        seeds = [key for key, item in self.first_links.items() if item is None]
+        for seed in seeds:
+            logger.info(f"Clicking through {seed}")
+            self._add_page(seed)
 
-  def save_web(self, file_name="wiki_web.tsv"):
-    ### Save the web as a tab-delimited file
-    logger.info("Saving results.")
-    with open(file_name, "w", newline="") as file:
-      writer = csv.writer(file, delimiter = "\t")
-      for key, value in self.first_links.items():
-        writer.writerow([key, value])
+    def load_web(self, file_name="wiki_web.tsv"):
+        # Loads a previously saved web. Helpful for adding more samples
+        logger.info(f"Loading {file_name}.")
+        with open(file_name, "r") as file:
+            reader = csv.reader(file, delimiter="\t")
+            n = 0
+            for row in reader:
+                self.first_links[row[0]] = row[1]
+                n += 1
+            logger.info(f"{n} urls added")
+
+    def save_web(self, file_name="wiki_web.tsv"):
+        # Save the web as a tab-delimited file
+        logger.info("Saving results.")
+        with open(file_name, "w", newline="") as file:
+            writer = csv.writer(file, delimiter="\t")
+            for key, value in self.first_links.items():
+                writer.writerow([key, value])
